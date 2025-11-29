c269dcdd9e9d764c7ee4c0cea0d927e923f89a26
rbxyz <rbcr4z1@gmail.com>
2025-11-29 00:51:44 -0300

    tentando subir

--- next.config.js
+++ next.config.js
@@ -3,20 +3,8 @@
  * for Docker builds.
  */
 import "./src/env.js";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import("next").NextConfig} */
-const config = {
-  experimental: {
-    // Pin Turbopack root to this workspace to avoid Next.js inferring the wrong root
-    // and ensure tools like Tailwind pick up the config from this project directory.
-    turbopack: {
-      root: __dirname,
-    },
-  },
-};
+const config = {};
 
 export default config;
